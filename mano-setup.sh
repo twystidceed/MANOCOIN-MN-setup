5e8c4f7fe4460747693e3aab6fd6da5c1550b7df
Twystidceed <kjwacikowski@gmail.com>
2020-12-12 03:06:41 -0500

    Update mano-setup.sh

--- mano-setup.sh
+++ mano-setup.sh
@@ -1,22 +1,78 @@
 #!/bin/bash
-# MANOCOIN Masternode Setup Script V1.0 for Ubuntu 16.04 LTS
+# Twystid's Masternode Setup Script V3.2 for Ubuntu
 #
 # Script will attempt to auto detect primary public IP address
-# and generate masternode private key unless specified in command line
-#
+# This script is capable of installing with or without swap depending on your VPS
 # Usage:
-# bash mano-setup-noswap.sh 
+# bash mano-setup.sh 
 #
 
 #Color codes
 RED='\033[0;91m'
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
 NC='\033[0m' # No Color
 
-#ports
+#TCP port
 PORT=12777
 RPC=12778
+
+#GLOBAL VARIABLES - Check the daemon deployment section for proper deployment
+
+#this is the Github Source for the binaries
+
+#Version for ubuntu 18.04
+SOURCEA=https://github.com/MANOPlatform/manocoin/releases/download/v1.0.0/mano-1.0.0-x86_64-linux-gnu.tar.gz
+
+#Version for ubuntu 16.04
+#SOURCEB=
+
+#The archive itself from the source
+#18.04
+ARCHIVEA=mano-1.0.0-x86_64-linux-gnu.tar.gz
+#16.04
+ARCHIVEB=MANO_Ubuntu_16_04.tar.gz
+
+#ADDNODES
+ADDNODEA=85.241.252.143:12777
+ADDNODEB=94.100.221.211:12777
+ADDNODEC=157.52.211.5:12777
+
+#name of the folder created with the git clone when clonign the repository
+FOLDER=MANOCOIN-MN-setup
+
+#official name
+NAME='Mano'
+
+#name2 is the actual name of the binary when installed on VPS [CASE SENSISTIVE]
+NAME2=mano
+
+#Simply the Ticker of the coin for referenceing in the script - no usage case not sensitive
+TICKER=MANO
+
+#actual name of the hidden folder for the coin [CASE SENSITIVE]
+HIDDEN=.manocoin
+
+#Actual name of the conf file in the hidden folder [CASE SENSITIVE]
+CONF=mano.conf
+
+#actual name od the coin daemon [CASE SENSITIVE]
+DAEMON=manod
+
+#Actual name of the coin daemon -cli [CASE SENSITIVE]
+CLI=mano-cli
+
+#name of the monitor script [CASE SENSITIVE]
+MONITOR=manomon.sh
+
+#only enable if needed due to binaries being extracted to a second folder within the cloned folder
+FOLDER2=mano-1.0.0/bin
+
+
+#####################################
+#     END OF GLOBAL VARIABLES       #
+#####################################
 
 #Clear keyboard input buffer
 function clear_stdin { while read -r -t 0; do read -r; done; }
@@ -26,44 +82,57 @@ function delay { echo -e "${GREEN}Sleep for $1 seconds...${NC}"; sleep "$1"; }
 
 #Stop daemon if it's already running
 function stop_daemon {
-    if pgrep -x 'manod' > /dev/null; then
-        echo -e "${YELLOW}Attempting to stop manod${NC}"
-        mano-cli stop
+    if pgrep -x $DAEMON > /dev/null; then
+        echo -e "${YELLOW}Attempting to stop $DAEMON${NC}"
+        $CLI stop
         delay 30
-        if pgrep -x 'manod' > /dev/null; then
-            echo -e "${RED}manod daemon is still running!${NC} \a"
+        if pgrep -x $DAEMON > /dev/null; then
+            echo -e "${RED}$DAEMON daemon is still running!${NC} \a"
             echo -e "${RED}Attempting to kill...${NC}"
-            pkill manod
+            pkill -9 $DAEMON
             delay 30
-            if pgrep -x 'manod' > /dev/null; then
-                echo -e "${RED}Can't stop manod! Reboot and try again...${NC} \a"
+            if pgrep -x $DAEMON > /dev/null; then
+                echo -e "${RED}Can't stop $DAEMON! Reboot and try again...${NC} \a"
                 exit 2
             fi
         fi
     fi
 }
 
-#Function detect_ubuntu
 
- if [[ $(lsb_release -d) == *16.04* ]]; then
-   UBUNTU_VERSION=16
-else
-   echo -e "${RED}You are not running Ubuntu 16.04, Installation is cancelled.${NC}"
-   exit 1
-
-fi
 
 
 #Process command line parameters
 genkey=$1
 
 clear
+echo -e "${PURPLE}####### #     # #     #  #####  ####### ### ######  ${NC}"
+echo -e "${PURPLE}   #    #  #  #  #   #  #     #    #     #  #     # ${NC}"
+echo -e "${PURPLE}   #    #  #  #   # #   #          #     #  #     # ${NC}"
+echo -e "${PURPLE}   #    #  #  #    #     #####     #     #  #     # ${NC}"
+echo -e "${PURPLE}   #    #  #  #    #          #    #     #  #     # ${NC}"
+echo -e "${PURPLE}   #    #  #  #    #    #     #    #     #  #     # ${NC}"
+echo -e "${PURPLE}   #     ## ##     #     #####     #    ### ######  ${NC}"
+echo -e
+echo -e "${PURPLE}      #     # ### #     # ### #     #  #####  ${NC}"
+echo -e "${PURPLE}      ##   ##  #  ##    #  #  ##    # #     # ${NC}"
+echo -e "${PURPLE}      # # # #  #  # #   #  #  # #   # #       ${NC}"
+echo -e "${PURPLE}      #  #  #  #  #  #  #  #  #  #  # #  #### ${NC}"
+echo -e "${PURPLE}      #     #  #  #   # #  #  #   # # #     # ${NC}"
+echo -e "${PURPLE}      #     #  #  #    ##  #  #    ## #     # ${NC}"
+echo -e "${PURPLE}      #     # ### #     # ### #     #  #####  ${NC}"
+echo -e
+echo -e "${GREEN}$NAME Masternode Setup Script V3 for Ubuntu LTS${NC}"
+echo -e
+echo -e "${GREEN}This script contains multiple options - please choose proper selections${NC}"
+echo -e
+echo -e 
+sleep 5
 
-echo -e "${YELLOW}MANO Masternode Setup Script V1 for Ubuntu 16.04 LTS${NC}"
-echo "Do you want me to generate a masternode private key for you? [y/n]"
+echo -e "${YELLOW}Do you want me to generate a masternode private key for you? [y/n]${NC}"
   read DOSETUP
 if [[ $DOSETUP =~ "n" ]] ; then
-          read -e -p "Enter your Masternode private key:" genkey;
+          read -e -p "Enter your private key:" genkey;
               read -e -p "Confirm your private key: " genkey2;
 fi
 
@@ -73,7 +142,7 @@ fi
 else 
      echo -e "${RED} Error: Private keys do not match. Try again or let me generate one for you...${NC} \a";exit 1
 fi
-sleep 1
+sleep .5
 clear
 
 # Determine primary public IP address
@@ -95,8 +164,6 @@ else
     fi
 fi
 #Check Deps
-sudo apt-get install -y unzip dtrx
-
 if [ -d "/var/lib/fail2ban/" ]; 
 then
     echo -e "${GREEN}Dependencies already installed...${NC}"
@@ -111,6 +178,8 @@ sudo apt-get -y install wget nano htop jq
 sudo apt-get -y install libzmq3-dev
 sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
 sudo apt-get -y install libevent-dev
+sudo apt-get install -y unzip
+sudo apt-get install -y dtrx
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get -y update
@@ -121,7 +190,7 @@ sudo service fail2ban restart
 sudo apt-get install -y libdb5.3++-dev libdb++-dev libdb5.3-dev libdb-dev && ldconfig
 sudo apt-get install -y libzmq3-dev build-essential libssl-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libboost-system1.58.0 libboost1.58-all-dev libdb4.8++ libdb4.8 libdb4.8-dev libdb4.8++-dev libevent-pthreads-2.0-5
    fi
-
+sudo apt-get install libboost-all-dev
 #Network Settings
 echo -e "${GREEN}Installing Network Settings...${NC}"
 {
@@ -158,112 +227,182 @@ echo -e "${NC}"
 } &> /dev/null
 echo -ne '[###################] (100%)\n'
 
-#Generating Random Password for LeisureCoind JSON RPC
+clear
+
+#Generating Random Password for tourd JSON RPC
 rpcuser=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-#Create 2GB swap file
-#if grep -q "SwapTotal" /proc/meminfo; then
-if grep -q "swapfile" /etc/fstab; then
-    echo -e "${GREEN}Skipping disk swap configuration...${NC} \n"
-else
-    echo -e "${YELLOW}Creating 2GB disk swap file. \nThis may take a few minutes!${NC} \a"
-    touch /var/swap.img
-    chmod 600 /var/swap.img
-    dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-    mkswap /var/swap.img 2> /dev/null
-    swapon /var/swap.img 2> /dev/null
-    if [ $? -eq 0 ]; then
-        echo '/var/swap.img none swap sw 0 0' >> /etc/fstab
-        echo -e "${GREEN}Swap was created successfully!${NC} \n"
-    else
-        echo -e "${RED}Operation not permitted! Optional swap was not created.${NC} \a"
-        rm /var/swap.img
-    fi
-fi
 
- #Extracting Daemon
-dtrx -n -f MANO_Ubuntu_16_04.tar.gz
-rm -rf MANO_Ubuntu_16_04.tar.gz
-rm mano-setup-noswap.sh
-rm mano-setup.sh
+echo -e "${YELLOW}=====================================================${NC}"
+echo -e "${YELLOW}=====================================================${NC}"
+echo -e 
+echo -e "${PURPLE}===========Optional SWAP Installation================${NC}"
+echo -e
+echo -e "${RED}Some providers do not allow you to install swap!${NC}"
+echo -e 
+echo -e "${GREEN}If you have VPS with locked swap select N to continue${NC}"
+echo -e
+echo -e "${GREEN}If you need to install SWAP or are unsure select Y${NC}"
+echo -e
+echo -e "${YELLOW}=====================================================${NC}"
+echo -e "${YELLOW}=====================================================${NC}"
+echo -e
+echo -e "Please enter your selection :"
+
+ read SWAP
+ 
+	if [[ $SWAP =~ "y" ]] ; then
+			echo "installing SWAP"
+			if grep -q "swapfile" /etc/fstab; then
+				echo -e "${GREEN}Skipping disk swap configuration...${NC} \n"
+			else
+				echo -e "${YELLOW}Creating 2GB disk swap file. \nThis may take a few minutes!${NC} \a"
+				touch /var/swap.img
+				chmod 600 /var/swap.img
+				dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+				mkswap /var/swap.img 2> /dev/null
+				swapon /var/swap.img 2> /dev/null
+				if [ $? -eq 0 ]; then
+					echo '/var/swap.img none swap sw 0 0' >> /etc/fstab
+					echo -e "${GREEN}Swap was created successfully!${NC} \n"
+				else
+					echo -e "${RED}Operation not permitted! Optional swap was not created.${NC} \a"
+					rm /var/swap.img
+				fi
+			fi
+	fi
+	clear
+
+####TEST FOR OPTION TO INSTALL FOR UBUNTU 18 OR 16 
+
+echo -e "${YELLOW}Detect Ubuntu Version${NC}"
+
+	if [[ $(lsb_release -d) == *18.04* ]]; then
+	UBUNTU_VERSION=18
+		#Extracting Daemon
+		cd ~/$FOLDER
+		sudo wget $SOURCEA
+		sudo dtrx -n -f $ARCHIVEA
+		rm -rf $ARCHIVEA
+		clear
+		echo -e
+		echo -e "${GREEN}Installing for Ubuntu Ver 18.04${NC}"
+		echo -e
+	elif [[ $(lsb_release -d) == *16.04* ]]; then
+		UBUNTU_VERSION=16
+			#Extracting Daemon
+			cd ~/$FOLDER
+			#sudo wget $SOURCEB
+			sudo dtrx -n -f $ARCHIVEB
+			rm -rf $ARCHIVEB
+			clear
+			echo -e
+			echo -e "${GREEN}Installing for Ubuntu Ver 16.04${NC}"
+			echo -e
+	else
+	echo -e "${RED}You are not running Ubuntu 18.04 or 16.04 - Installation is cancelled.${NC}"
+	echo -e
+	echo -e "${RED}Please choose a VPS with Ubuntu 16.04 or 18.04 to use this script!${NC}"
+	
+	exit 1
+
+	fi
+	
+#### END TEST FOR VERSION
+
+
+
  
  stop_daemon
  
  # Deploy binaries to /usr/bin
- sudo cp mano* /usr/bin/
- sudo chmod 755 -R ~/MANOCOIN-MN-setup
- sudo chmod 755 /usr/bin/mano*
+ cd ~/$FOLDER/$FOLDER2/
+ sudo cp $NAME2* /usr/bin/
+ sudo chmod 755 -R ~/$FOLDER
+ sudo chmod 755 /usr/bin/$NAME2*
  
  # Deploy masternode monitoring script
- cp ~/MANOCOIN-MN-setup/manomon.sh /usr/local/bin/
- sudo chmod 711 /usr/local/bin/manomon.sh
+ cp ~/$FOLDER/$MONITOR /usr/local/bin/
+ sudo chmod 711 /usr/local/bin/$MONITOR
  
  #Create datadir
- if [ ! -f ~/.manocoin/mano.conf ]; then 
- 	sudo mkdir ~/.manocoin
+ if [ ! -f ~/$HIDDEN/$CONF ]; then 
+ 	sudo mkdir ~/$HIDDEN
  fi
 
-echo -e "${YELLOW}Creating mano.conf...${NC}"
+echo -e "${YELLOW}Creating $CONF...${NC}"
 
 # If genkey was not supplied in command line, we will generate private key on the fly
 if [ -z $genkey ]; then
-    cat <<EOF > ~/.manocoin/mano.conf
+    cat <<EOF > ~/$HIDDEN/$CONF
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
 EOF
 
-    sudo chmod 755 -R ~/.manocoin/mano.conf
+    sudo chmod 755 -R ~/$HIDDEN/$CONF
 
     #Starting daemon first time just to generate masternode private key
-    manod -daemon
-echo -ne '[##                 ] (15%)\r'
-sleep 6
-echo -ne '[######             ] (30%)\r'
-sleep 9
-echo -ne '[########           ] (45%)\r'
-sleep 6
-echo -ne '[##############     ] (72%)\r'
+    $DAEMON -daemon
+echo -ne '[#         ] (10%)\r'
 sleep 10
-echo -ne '[###################] (100%)\r'
+echo -ne '[##        ] (20%)\r'
+sleep 10
+echo -ne '[###       ] (30%)\r'
+sleep 10 
+echo -ne '[####      ] (40%)\r'
+sleep 10
+echo -ne '[#####     ] (50%)\r'
+sleep 10
+echo -ne '[######    ] (60%)\r'
+sleep 10
+echo -ne '[#######   ] (70%)\r'
+sleep 10
+echo -ne '[########  ] (80%)\r'
+sleep 10
+echo -ne '[######### ] (90%)\r'
+sleep 10
+echo -ne '[##########] (100%)\r'
 echo -ne '\n'
 
     #Generate masternode private key
     echo -e "${YELLOW}Generating masternode private key...${NC}"
-    genkey=$(mano-cli masternode genkey)
+    genkey=$($CLI masternode genkey)
     if [ -z "$genkey" ]; then
         echo -e "${RED}ERROR: Can not generate masternode private key.${NC} \a"
         echo -e "${RED}ERROR: Reboot VPS and try again or supply existing genkey as a parameter.${NC}"
         exit 1
     fi
     
-    #Stopping daemon to create mano.conf
+    #Stopping daemon to create $CONF
     stop_daemon
 fi
 
-# Create mano.conf
-cat <<EOF > ~/.manocoin/mano.conf
+# Create $CONF
+cat <<EOF > ~/$HIDDEN/$CONF
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
 rpcport=$RPC
 rpcallowip=127.0.0.1
-onlynet=ipv4
 listen=1
 server=1
 daemon=1
-masternode=1
 logintimestamps=1
 maxconnections=10
 externalip=$publicip:$PORT
+masternode=1
 masternodeprivkey=$genkey
+addnode=$ADDNODEA
+addnode=$ADDNODEB
+addnode=$ADDNODEC
 EOF
 
-#Finally, starting daemon with new mano.conf
-manod -daemon
+#Finally, starting daemon with new $CONF
+$DAEMON -daemon
 delay 10
 
-#Setting auto start cron job for manod
-cronjob="@reboot sleep 30 && manod -daemon"
+#Setting auto start cron job daemon
+cronjob="@reboot sleep 30 && $DAEMON -daemon"
 crontab -l > tempcron
 if ! grep -q "$cronjob" tempcron; then
     echo -e "${GREEN}Configuring crontab job...${NC}"
@@ -278,7 +417,7 @@ ${YELLOW}Masternode setup is complete!${NC}
 Masternode was installed with VPS IP Address: ${YELLOW}$publicip${NC}
 Masternode Private Key: ${YELLOW}$genkey${NC}
 Now you can add the following string to the masternode.conf file
-for your Hot Wallet (the wallet with your MANO collateral funds):
+for your Hot Wallet (the wallet with your $TICKER collateral funds):
 ======================================================================== \a"
 echo -e "${YELLOW}mn1 $publicip:$PORT $genkey TxId TxIdx${NC}"
 echo -e "========================================================================
@@ -289,7 +428,7 @@ into your ${YELLOW}masternode.conf${NC} file and replace:
     ${YELLOW}TxId${NC} - with Transaction Id from masternode outputs
     ${YELLOW}TxIdx${NC} - with Transaction Index (0 or 1)
      Remember to save the masternode.conf and restart the wallet!
-To introduce your new masternode to the MANO network, you need to
+To introduce your new masternode to the $TICKER network, you need to
 issue a masternode start command from your wallet, which proves that
 the collateral for this node is secured."
 
@@ -303,7 +442,7 @@ from several minutes to several hours depending on the network state.
 Your initial Masternode Status may read:
     ${YELLOW}Node just started, not yet activated${NC} or
     ${YELLOW}Node  is not in masternode list${NC}, which is normal and expected.
-2) Wait at least until 'IsBlockchainSynced' status becomes 'true'.
+2) Wait at least until 'IsBlockchainSynced' status becomes 'true' or status 999.
 At this point you can go to your wallet and issue a start
 command by either using Debug Console:
     Tools->Debug Console-> enter: ${YELLOW}masternode start-alias mn1${NC}
@@ -316,7 +455,7 @@ Once completed step (2), return to this VPS console and wait for the
 Masternode Status to change to: 'Masternode successfully started'.
 This will indicate that your masternode is fully functional and
 you can celebrate this achievement!
-Currently your masternode is syncing with the LSR network...
+Currently your masternode is syncing with the $TICKER network...
 The following screen will display in real-time
 the list of peer connections, the status of your masternode,
 node synchronization status and additional network and node stats.
@@ -328,39 +467,38 @@ echo -e "
 ${GREEN}...scroll up to see previous screens...${NC}
 Here are some useful commands and tools for masternode troubleshooting:
 ========================================================================
-To view masternode configuration produced by this script in mano.conf:
-${YELLOW}cat ~/.manocoin/mano.conf${NC}
-Here is your mano.conf generated by this script:
+To view masternode configuration produced by this script in $CONF:
+${YELLOW}cat ~/$HIDDEN/$CONF${NC}
+Here is your $CONF generated by this script:
 -------------------------------------------------${YELLOW}"
-cat ~/.manocoin/mano.conf
+cat ~/$HIDDEN/$CONF
 echo -e "${NC}-------------------------------------------------
-NOTE: To edit mano.conf, first stop the manod daemon,
-then edit the mano.conf file and save it in nano: (Ctrl-X + Y + Enter),
-then start the manod daemon back up:
-             to stop:   ${YELLOW}mano-cli stop${NC}
-             to edit:   ${YELLOW}nano ~/.manocoin/mano.conf${NC}
-             to start:  ${YELLOW}manod${NC}
+NOTE: To edit $CONF, first stop the $DAEMON daemon,
+then edit the $CONF file and save it in nano: (Ctrl-X + Y + Enter),
+then start the $DAEMON daemon back up:
+             to stop:   ${YELLOW}$CLI stop${NC}
+             to edit:   ${YELLOW}nano ~/$HIDDEN/$CONF${NC}
+             to start:  ${YELLOW}$DAEMON -daemon{NC}
 ========================================================================
-To view MANO debug log showing all MN network activity in realtime:
-             ${YELLOW}tail -f ~/.manocoin/debug.log${NC}
+To view LSR debug log showing all MN network activity in realtime:
+             ${YELLOW}tail -f ~/$HIDDEN/debug.log${NC}
 ========================================================================
 To monitor system resource utilization and running processes:
                    ${YELLOW}htop${NC}
 ========================================================================
 To view the list of peer connections, status of your masternode, 
-sync status etc. in real-time, run the manomon.sh script:
-                 ${YELLOW}manomon.sh${NC}
+sync status etc. in real-time, run the $MONITOR script:
+                 ${YELLOW}$MONITOR${NC}
 or just type 'node' and hit <TAB> to autocomplete script name.
 ========================================================================
-Enjoy your MANO Masternode and thanks for using this setup script!
+Enjoy your $TICKER Masternode and thanks for using this setup script!
 If you found this script useful, please donate to : 
 ${GREEN}no donations at this time ${NC}
 ...and make sure to check back for updates!
+Contact Twystidceed#4126 on discord if you need additional support
 "
-delay 30
-# Run manomon.sh
-manomon.sh
+delay 10
+# Run $MONITOR
+sudo $MONITOR
 
 # EOF
-
-
